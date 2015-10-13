962b45a9fff8bae4af3fc1a504a168fbc929b20c
SHI FU CHEN <shifuchen@me.com>
2015-10-13 16:09:30 +0800

    Bug fixed

--- static/js/app.js
+++ static/js/app.js
@@ -3,17 +3,17 @@ var main = function () {
   $('.content').click(function () { // 点击内容
     $('.barrier').fadeIn(300); // 半透明图层逐渐出现
     $('.peekbar').animate({ top: "50%" }, 300); // PeekBar 滑入
-    $('.main').animate({ width: "95%", top: "2.5%", left: "2.5%" }, 300); // 主内容区域缩小
+    $('.main').animate({ zoom: "0.95", width: "95%", top: "2.5%", left: "2.5%" }, 300); // 主内容区域缩小
   });
   $('.barrier').click(function () { // 点击 PeekBar 以外的区域
     $('.barrier').fadeOut(300); // 半透明图层逐渐隐藏
     $('.peekbar').animate({ top: "100%" }, 300); // PeekBar 滑出
-    $('.main').animate({ width: "100%", top: "0%", left: "0%" }, 300); // 主内容区域放大
+    $('.main').animate({ zoom: "1", width: "100%", top: "0%", left: "0%" }, 300); // 主内容区域放大
   });
   $(document.getElementById("done")).click(function () { // 点击 Done 按钮
     $('.barrier').fadeOut(300); // 半透明图层逐渐隐藏
     $('.peekbar').animate({ top: "100%" }, 300); // PeekBar 滑出
-    $('.main').animate({ width: "100%", top: "0%", left: "0%" }, 300); // 主内容区域放大
+    $('.main').animate({ zoom: "1", width: "100%", top: "0%", left: "0%" }, 300); // 主内容区域放大
   });
 };
 
