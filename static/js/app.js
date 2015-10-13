3957301f5aabf9df75a495098ddeb2bae586c268
SHI FU CHEN <shifuchen@me.com>
2015-10-13 16:03:54 +0800

    New feature added
    Now PeekBar is allowed to be dismissed by clicking on the area outside
    it.

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,11 @@ var main = function () {
     $('.peekbar').animate({ top: "50%" }, 300); // PeekBar 滑入
     $('.main').animate({ width: "95%", top: "2.5%", left: "2.5%" }, 300); // 主内容区域缩小
   });
+  $('.barrier').click(function () { // 点击 PeekBar 以外的区域
+    $('.barrier').fadeOut(300); // 半透明图层逐渐隐藏
+    $('.peekbar').animate({ top: "100%" }, 300); // PeekBar 滑出
+    $('.main').animate({ width: "100%", top: "0%", left: "0%" }, 300); // 主内容区域放大
+  });
   $(document.getElementById("done")).click(function () { // 点击 Done 按钮
     $('.barrier').fadeOut(300); // 半透明图层逐渐隐藏
     $('.peekbar').animate({ top: "100%" }, 300); // PeekBar 滑出
