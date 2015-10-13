21e892135320568bc7f2bbb9091990e3ae29a3f4
SHI FU CHEN <shifuchen@me.com>
2015-10-13 15:49:50 +0800

    Shadow disabled

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,16 @@
-var main = function() {
+var main = function () {
   "use strict";
-  $('.content').click(function() {
+  $('.content').click(function () {
     $('.barrier').fadeIn(300);
-    $('.peekbar').animate({top: "50%"},300);
-    $('.main').animate({width: "95%", top: "2.5%", left: "2.5%"},300);
+    $('.peekbar').animate({ top: "50%" }, 300);
+    $('.main').animate({ width: "95%", top: "2.5%", left: "2.5%" }, 300);
 
-    
+
   });
-  $(document.getElementById("done")).click(function() {
+  $(document.getElementById("done")).click(function () {
     $('.barrier').fadeOut(300);
-    $('.peekbar').animate({top: "100%"},300);
-    $('.main').animate({width: "100%", top: "0%", left: "0%"},300);
+    $('.peekbar').animate({ top: "100%" }, 300);
+    $('.main').animate({ width: "100%", top: "0%", left: "0%" }, 300);
   });
 };
 
